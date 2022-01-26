097957bba7fb8b4d0a94447afa541a48e6453483
Nevin Kaplan <n.kaplan@castsoftware.com>
2022-01-26 13:30:59 -0500

    Version 1.4.5 changes

--- README.md
+++ README.md
@@ -19,6 +19,69 @@ The script is designed to run on the command line using one parameter, --config,
 
     py convert.py --config config.properties 
 
+# Configuration file
+The Assessment Deck Generation Tool is configured using a json formatted file.  The file is divided into three sections, general configuration, application list and rest configuration.  
+#### General Configuration
+The general configuration section contains four parts,  
+* company - the name of the company as it will appear on the report
+* project name - the name of the project as it will appear on the report
+* template - the absolute location of the report template
+* output - the absolute location of the report output folder
+
+#### Application list
+This section contains a list of all applications that will be used in the report.  It is divided into three parts,  
+* aip - the application schema triplet base
+* highlight - the name of the application as it appears in the Highlight portal
+* title - the name of the application to be used in the report
+
+aip is the only required in this section.  If Highlight or title are empty or left out the aip value will be used.
+
+#### REST Configuration
+The REST configuration is divided into two parts, AIP and Highlight.  Both configurations contain 
+* Active - toggle to turn REST service on/off
+* URL - the service base URL 
+* user - login user id
+* password - login password (non-encrypted)
+
+The Highlight configuration contains an additional field, instance, which refers to the login instance id.
+
+#### Sample configuration
+
+    {
+        "company":"MMA Company Name",
+        "project":"Project Name",
+        "template":"C:/com.castsoftware.uc.arg/Template.pptx",
+        "output":"C:/output/folder",
+        "application":[
+            {
+                "aip":"triplet_base_name",
+                "highlight":"highlight_name",
+                "title":"application title"
+            },
+            {
+                "aip":"triplet_base_name",
+                "highlight":"highlight_name",
+                "title":"application title"
+            }, ...
+        ],
+        "rest":{
+            "AIP":{
+                "Active":true,
+                "URL":"http://<URL>:<Port>/rest/",
+                "user":"user_name",
+                "password":"user_pasword"
+            },
+            "Highlight":{
+                "Active":true,
+                "URL":"https://<URL>/WS2/",
+                "user":"user_name",
+                "password":"user_pasword",
+                "instance":"instance id"
+            }
+        }
+    }
+
+
 ### Output
 A single PowerPoint deck and one excel spread sheet is generated for each application configured in the properties file. The Deck is organized with an executive summary, one section per application containing detailed information and an appendix.  The excel sheets are hold the application action plan information and divided into two tabs, summary, and detail.  In the event an action plan is not configured, for the application, the sheets will be empty.    
 
---
+++ hlRestCall.py
@@ -0,0 +1,130 @@
+from restAPI import RestCall
+from requests import codes
+from pandas import DataFrame
+from pandas import json_normalize
+from pandas import concat
+
+
+class HLRestCall(RestCall):
+    """
+    Class to handle HL REST API calls.
+    """
+    def __init__(self, hl_base_url, hl_user, hl_pswd, hl_instance, timer_on=False):
+        super().__init__(hl_base_url, hl_user, hl_pswd, timer_on)
+
+        self._hl_instance = hl_instance
+        self._hl_data_retrieved = False
+    
+    def _get_app_ids(self, instance_id):
+        # Retrieve the HL app id for the application.
+
+        try:
+            # TODO: remove the hard-coding
+            # TODO: Get the app id.
+            url = f'domains/{instance_id}/applications'
+
+            (status, json) = self.get(url,headers={'Accept': '*/*'})
+
+            # TODO: Handle exceptions
+            if status == codes.ok and len(json) > 0:
+                pass
+        except:
+            # TODO
+            print('Oopsi.. caught an exception')
+            raise
+
+        return json
+
+    def get_appl_third_party(self,appl_id):
+        url = f'domains/{self._hl_instance}/applications/{appl_id}/thirdparty'
+        (status, json) = self.get(url)
+        if status == codes.ok and len(json) > 0:
+            return json
+        else:
+            raise KeyError (f'Application not found {appl_id}')
+
+
+    def get_appls(self):
+        url = f'domains/{self._hl_instance}/applications/'
+        (status, json) = self.get(url)
+        if status == codes.ok and len(json) > 0:
+            return json
+        else:
+            raise KeyError (f'No applications not found')
+
+    def get_app_id(self,app_name):
+        url = f'domains/{self._hl_instance}/applications/'
+        (status, json) = self.get(url)
+
+        # TODO: Handle exceptions
+        if status == codes.ok and len(json) > 0:
+            for id in json:
+                if id['name'].lower()==app_name.lower():
+                    return id['id']
+            raise KeyError (f'Application not found: {app_name}')
+
+    def get_cloud_data(self,app_id):
+        url = f'domains/{self._hl_instance}/applications/{app_id}'
+        (status, json) = self.get(url)
+        if status == codes.ok and len(json) > 0:
+            rslt = DataFrame()
+            try:
+                cloud_data = json['metrics'][0]['cloudReadyDetail']
+                for d in cloud_data:
+                    cd = json_normalize(d['cloudReadyDetails'])
+                    cd['Technology']=d['technology']
+                    cd['Scan']=d['cloudReadyScan']
+                    rslt = concat([rslt,cd],ignore_index=True)
+            except KeyError as e:
+                self.warning('Error retrieving cloud ready information')
+            return rslt
+        else: 
+            return None
+
+
+    def get_third_party(self, app_id):
+        cves = DataFrame()
+        lic = DataFrame()
+
+        url = f'domains/{self._hl_instance}/applications/{app_id}/thirdparty'
+        (status, json) = self.get(url)
+
+        third_party = []
+        if status == codes.ok and len(json) > 0:
+            third_party = json['thirdParties']
+            for tp in third_party:
+                if 'cve' in tp:
+                    cve_df = json_normalize(tp['cve']['vulnerabilities'])
+                    cve_df.rename(columns={'name':'cve'},inplace=True)
+                    
+                    cve_df['component']=tp['name']
+                    cve_df['version']=tp['version']
+                    cve_df['languages']=tp['languages']
+                    cve_df['release']=tp['release']
+                    cve_df['origin']=tp['origin']
+                    cve_df['lastVersion']=tp['lastVersion']
+
+                    cves=concat([cves,cve_df],ignore_index=True)
+
+                if 'licenses' in tp:
+                    lic_df = json_normalize(tp['licenses'], \
+                        meta = ['name','version','languages','release','origin','lastVersion'])
+                    lic_df.rename(columns={'name':'license'},inplace=True)
+                    lic_df['component']=tp['name']
+                    load_df_element(tp,lic_df,'version')
+                    load_df_element(tp,lic_df,'languages')
+                    load_df_element(tp,lic_df,'release')
+                    load_df_element(tp,lic_df,'origin')
+                    load_df_element(tp,lic_df,'lastVersion')
+                    lic=concat([lic,lic_df],ignore_index=True)
+
+            if 'component' in cves.columns:
+                cves=cves[['component','version','languages','release','origin','lastVersion','cve', 'description', 'cweId', 'cweLabel', 'criticity', 'cpe']]
+            if 'component' in lic.columns:
+                lic=lic[['component','version','languages','release','origin','lastVersion','license','compliance']] 
+
+        return lic,cves,len(third_party)
+
+def load_df_element(src,dst,name):
+    if not (src.get(name) is None):
+        dst[name]=src[name] 
